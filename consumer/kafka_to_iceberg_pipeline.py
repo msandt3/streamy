37639c82117b5ef84817cfebb47e2e1e8eca1997
Michael Sandt <michael.sandt@gmail.com>
2025-09-16 20:07:14 -0400

    WIP - Add pipeline to push kafka events to local filesystem in iceberg format. Gotcha here - there's a few version conflicts with the dependencies. Noted in requirements.txt

---
+++ consumer/kafka_to_iceberg_pipeline.py
@@ -0,0 +1,20 @@
+import dlt
+from lib.kafka_consumer import kafka_consumer
+from dlt.destinations import filesystem
+
+
+def kafka_to_filesystem_iceberg() -> None:
+    pipeline = dlt.pipeline(
+        pipeline_name="wikipedia_events",
+        destination="filesystem",
+        dataset_name="wiki_events"
+    )
+
+    for batch in kafka_consumer('test-topic'):
+        pipeline.run(batch, table_name="kafka_messages", table_format="iceberg")
+    
+    
+
+if __name__ == "__main__":
+    dlt.config['destination.parquet.store_decimal_as_integer'] = False
+    kafka_to_filesystem_iceberg()
